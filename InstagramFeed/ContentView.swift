8c741ea28b67ade7952ff89d58afc4872492d917
Stephan Dowless <dowless.stephan@gmail.com>
2020-02-27 20:00:05 -0500

    Initial Commit

---
+++ InstagramFeed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramFeed
+//
+//  Created by Stephen Dowless on 2/27/20.
+//  Copyright © 2020 Stephan Dowless. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
